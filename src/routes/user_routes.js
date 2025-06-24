e2afd8d11764301875b3b5eb1b069839035057b7
Marlon Ramos <marlonrdacosta@gmail.com>
2025-06-24 19:09:09 -0300

    feat: dynamic update query

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,9 +1,16 @@
 import {Router} from 'express'
 import userController from '../controller/user_controllers.js'
-
+import {validate, validateUserId} from '../middlewares/validation_middlewares.js'
+import {userSchema} from '../schema/user_schema.js'
 const router = Router()
 
 
-router.post('/users', userController.createUserController)
+router.post('/users',validate(userSchema), userController.createUserController)
+router.get("/users", userController.findAllUserController)
+router.get("/users/:id",validateUserId, userController.findUserByIdController)
+router.patch("/users/:id",validateUserId, userController.updateUserController)
+router.delete("/users/:id", validateUserId, userController.deleteUserController)
+
+
 
 export default router
---
+++ src/schema/user_schema.js
@@ -0,0 +1,17 @@
+import {z} from 'zod'
+
+const userSchema = z.object({
+    username: z.string().min(3, 'Username is required'),
+    email: z.string().email('Invalid Email'),
+    password: z.string().min(6, 'Password must have at least 6 caracteres'),
+    avatar: z.string().url('Invalid URL').optional()
+})
+
+const userIdSchema = z.object({
+    userId: z.number().int().positive('User ID must be a positive integer')
+})
+
+
+
+export { userSchema, userIdSchema }
+
